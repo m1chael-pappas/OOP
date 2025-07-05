02ebac59c7a664b1fd8e687210236cdd4a351dbe
MichaelPappas2662 <mixalis.pappas23@gmail.com>
2025-07-05 11:10:42 +1000

    adding some move apps

---
+++ Repetition/Repetition.cs
@@ -0,0 +1,25 @@
+﻿
+
+class Repetition
+{
+    public static void Main()
+    {
+        int sum = 0;
+        double average = 0;
+        int upperbound = 100;
+
+        int number = 1;
+        do
+        {
+            sum += number;
+            number++;
+            Console.WriteLine($"Current number: {number}, Current sum: {sum}");
+        } while (number <= upperbound);
+        if (upperbound > 0)
+        {
+            average = (double)sum / upperbound;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+    }
+}
